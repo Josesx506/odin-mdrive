7f7d999ceb41fe48d2e4da11fa5331e48c7a0e0b
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-03-27 15:02:30 -0400

    manually migrating the db

--- resetPrisma.sh
+++ resetPrisma.sh
@@ -1,4 +1,5 @@
 rm -rf prisma/migrations/
 mkdir -p prisma/migrations/0_init
 npx prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql
+psql $DATABASE_URL -f prisma/migrations/0_init/migration.sql
 npx prisma migrate resolve --applied 0_init
