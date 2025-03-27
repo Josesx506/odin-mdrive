c56abf230a6352eb7b633f472333ae4116a602ed
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-03-27 15:26:57 -0400

    manually migrating the db

--- resetPrisma.sh
+++ resetPrisma.sh
@@ -1,6 +1,8 @@
 rm -rf prisma/migrations/
-# mkdir -p prisma/migrations/0_init
-# npx prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql
-# psql $DATABASE_URL -f prisma/migrations/0_init/migration.sql
-# npx prisma migrate resolve --applied 0_init
-npx prisma migrate dev --name init_mig
+mkdir -p prisma/migrations/0_init
+npx prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql
+apt update && apt install -y postgresql-client
+psql $DATABASE_URL -f prisma/migrations/0_init/migration.sql
+npx prisma migrate resolve --applied 0_init
+# npx prisma migrate dev --name 0_init_mig
+# npx prisma migrate resolve --applied 0_init_mig
