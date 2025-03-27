69f44962c800785b7c0a95fb1ed68c9d170462ca
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-03-27 01:49:51 -0400

    baseline prisma migration

--- prisma/migrations/20250325070848_drive_user_updated_columns/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `dirName` on the `DriveUser` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "DriveUser" DROP COLUMN "dirName";
--- prisma/migrations/20250325071034_drive_item_default_zero_value_for_file_size_column/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "DriveItem" ALTER COLUMN "fileSize" SET DEFAULT 0;
